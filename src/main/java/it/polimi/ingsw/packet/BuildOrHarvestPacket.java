61622aa1ebb8bcc0c48bdb4fa8df17b44b7ca7d0
Federico Amadelli <federico.amadelli@mail.polimi.it>
2017-05-16 16:26:46 +0200

    packet modify

---
+++ src/main/java/it/polimi/ingsw/packet/BuildOrHarvestPacket.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.packet;
+
+/**
+ * Created by federico on 16/05/2017.
+ */
+public class BuildOrHarvestPacket extends MovePacket{
+    private
+}
---
+++ src/main/java/it/polimi/ingsw/packet/MovePacket.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.packet;
+
+import it.polimi.ingsw.gamelogic.FamilyMember;
+import it.polimi.ingsw.gamelogic.FamilyMemberColor;
+
+import java.io.Serializable;
+
+/**
+ * Created by federico on 16/05/2017.
+ */
+public class MovePacket implements Serializable {
+    private FamilyMemberColor familyMemberColor;
+    private int servantUsed;
+    public MovePacket(FamilyMemberColor familyMemberColor, int servantUsed){
+        this.familyMemberColor=familyMemberColor;
+        this.servantUsed=servantUsed;
+    }
+
+    public FamilyMemberColor getFamilyMemberColor() {
+        return familyMemberColor;
+    }
+
+    public int getServantUsed() {
+        return servantUsed;
+    }
+}
