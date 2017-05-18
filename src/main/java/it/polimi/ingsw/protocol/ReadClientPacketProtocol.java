1e5879f5ca432bd734056dae2c5523574b4d00cd
Federico Amadelli <federico.amadelli@mail.polimi.it>
2017-05-18 20:26:16 +0200

    start to use of the lambda function

--- src/main/java/it/polimi/ingsw/protocol/ReadClientPacketProtocol.java
+++ src/main/java/it/polimi/ingsw/protocol/ReadClientPacketProtocol.java
@@ -13,7 +13,7 @@ public class ReadClientPacketProtocol {
     /**
      * the different istruction to read the packet
      */
-    private HashMap<PacketType,SocketPlayer> istruction;
+    private HashMap<PacketType, FunctionResponse> istruction;
     public ReadClientPacketProtocol(SocketPlayer player){
         this.player=player;
         //putIstruction();
@@ -22,18 +22,18 @@ public class ReadClientPacketProtocol {
     /**
      * this method is used to save all the response based on the packetType
      */
-    /*private void putIstruction(){
-        istruction.put(PacketType.LOGIN, );
-        istruction.put(PacketType.REGISTER, );
-        istruction.put(PacketType.MOVE_IN_TOWER, );
-        istruction.put(PacketType.MOVE_IN_MARKET, );
-        istruction.put(PacketType.HARVESTING, );
-        istruction.put(PacketType.BUILDING, );
-        istruction.put(PacketType.DISCARD_LEADER, );
-        istruction.put(PacketType.PLAY_LEADER, );
-        istruction.put(PacketType.CHAT, );
-        istruction.put(PacketType.END_PHASE, );
-    }*/
+    private void putIstruction(){
+        istruction.put(PacketType.LOGIN, ()-> player.);
+        istruction.put(PacketType.REGISTER, ()-> player.);
+        istruction.put(PacketType.MOVE_IN_TOWER, ()-> player.);
+        istruction.put(PacketType.MOVE_IN_MARKET, ()-> player.);
+        istruction.put(PacketType.HARVESTING, ()-> player.);
+        istruction.put(PacketType.BUILDING, ()-> player.);
+        istruction.put(PacketType.DISCARD_LEADER, ()-> player.);
+        istruction.put(PacketType.PLAY_LEADER, ()-> player.);
+        istruction.put(PacketType.CHAT, ()-> player.floodChatMsg());
+        istruction.put(PacketType.END_PHASE, ()-> player.);
+    }
 
 
 }
