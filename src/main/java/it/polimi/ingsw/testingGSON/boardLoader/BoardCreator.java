249c1efae7e185db4630bd24e94dfc64f47ee0b5
Luca Campanella <luca.campanella@mail.polimi.it>
2017-05-23 16:44:19 +0200

    Added RuntimeTypeAdapterFactory class

--- src/main/java/it/polimi/ingsw/testingGSON/boardLoader/BoardCreator.java
+++ src/main/java/it/polimi/ingsw/testingGSON/boardLoader/BoardCreator.java
@@ -3,8 +3,12 @@ package it.polimi.ingsw.testingGSON.boardLoader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import it.polimi.ingsw.client.CliPrinter;
-import it.polimi.ingsw.model.board.*;
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.effects.EffectInterface;
+import it.polimi.ingsw.model.effects.NoEffect;
+import it.polimi.ingsw.model.effects.TakeOrPaySomethingEffect;
 import it.polimi.ingsw.utils.Debug;
+
 import java.io.InputStreamReader;
 import java.io.Reader;
 
@@ -16,11 +20,19 @@ public class BoardCreator {
     public static void main(String[] args) throws Exception {
         Debug.instance(Debug.LEVEL_VERBOSE);
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(TowerFloorAS.class, new TowerFloorDeserializer());
+        /*gsonBuilder.registerTypeAdapter(TowerFloorAS.class, new TowerFloorDeserializer());
         gsonBuilder.registerTypeAdapter(MarketAS.class, new MarketASDeserializer());
         gsonBuilder.registerTypeAdapter(CouncilAS.class, new CouncilDeserializer());
 
-        Gson gson = gsonBuilder.create();
+        Gson gson = gsonBuilder.create();*/
+
+        RuntimeTypeAdapterFactory<EffectInterface> runtimeTypeAdapterFactory = RuntimeTypeAdapterFactory.of(EffectInterface.class, "effectName");
+        runtimeTypeAdapterFactory.registerSubtype(NoEffect.class, "NoEffect");
+        runtimeTypeAdapterFactory.registerSubtype(TakeOrPaySomethingEffect.class, "TakeOrPaySomethingEffect");
+
+        Gson gson = gsonBuilder.registerTypeAdapterFactory(runtimeTypeAdapterFactory).create();
+
+
         // The JSON data
         try (Reader reader = new InputStreamReader(BoardCreator.class.getResourceAsStream("/BoardCFG.json"), "UTF-8")) {
             Board board = gson.fromJson(reader, Board.class);
