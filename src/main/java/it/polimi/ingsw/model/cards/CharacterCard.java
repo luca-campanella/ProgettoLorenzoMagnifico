d1405820d47444af5e77c17e825f46028e5caa31
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2017-05-23 16:51:14 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/it/polimi/ingsw/client/CliPrinter.java
    #	src/main/java/it/polimi/ingsw/controller/BoardConfigurator.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/Board.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/BuildAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/CardColorEnum.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/CouncilAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/HarvestAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/Market.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/MarketAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/Tower.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/TowerFloorAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/Board/VaticanReport.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/Board.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/BuildAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/CardColorEnum.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/CouncilAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/HarvestAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/Market.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/MarketAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/Tower.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/TowerFloorAS.java
    #	src/main/java/it/polimi/ingsw/gamelogic/board/VaticanReport.java
    #	src/main/java/it/polimi/ingsw/model/board/AbstractActionSpace.java
    #	src/main/java/it/polimi/ingsw/model/board/Board.java
    #	src/main/java/it/polimi/ingsw/model/board/BuildAS.java
    #	src/main/java/it/polimi/ingsw/model/board/CardColorEnum.java
    #	src/main/java/it/polimi/ingsw/model/board/CouncilAS.java
    #	src/main/java/it/polimi/ingsw/model/board/HarvestAS.java
    #	src/main/java/it/polimi/ingsw/model/board/Market.java
    #	src/main/java/it/polimi/ingsw/model/board/MarketAS.java
    #	src/main/java/it/polimi/ingsw/model/board/Tower.java
    #	src/main/java/it/polimi/ingsw/model/board/TowerFloorAS.java
    #	src/main/java/it/polimi/ingsw/model/board/VaticanReport.java
    #	src/main/java/it/polimi/ingsw/model/controller/GameController.java
    #	src/main/java/it/polimi/ingsw/server/boardLoader/BuildDeserializer.java
    #	src/main/java/it/polimi/ingsw/server/boardLoader/CouncilDeserializer.java
    #	src/main/java/it/polimi/ingsw/server/boardLoader/EffectParser.java
    #	src/main/java/it/polimi/ingsw/server/boardLoader/HarvestDeserializer.java
    #	src/main/java/it/polimi/ingsw/server/boardLoader/MarketASDeserializer.java
    #	src/main/java/it/polimi/ingsw/testingGSON/boardLoader/BoardCreator.java
    #	src/main/java/it/polimi/ingsw/testingGSON/boardLoader/TowerFloorDeserializer.java

---
+++ src/main/java/it/polimi/ingsw/model/cards/CharacterCard.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.cards;
+
+/**
+ * Created by higla on 23/05/2017.
+ */
+public class CharacterCard {
+}
---
+++ src/main/java/it/polimi/ingsw/model/cards/Deck.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model.cards;
+
+import java.util.ArrayList;
+
+/**
+ * Created by higla on 23/05/2017.
+ */
+public class Deck {
+    private ArrayList<AbstractCard> allCards;
+
+    public void shuffle(){
+        ;
+    }
+    public void getNextCard(){
+        ;
+    }
+}
