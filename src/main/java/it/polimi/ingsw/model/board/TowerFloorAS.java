20da406a75546280e128f0f9297ba8bfc763b8b9
Federico Amadelli <federico.amadelli@mail.polimi.it>
2017-05-29 11:49:16 +0200

    add game logic

--- src/main/java/it/polimi/ingsw/model/board/TowerFloorAS.java
+++ src/main/java/it/polimi/ingsw/model/board/TowerFloorAS.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model.board;
 
+import it.polimi.ingsw.model.cards.AbstractCard;
+import it.polimi.ingsw.model.effects.immediateEffects.ImmediateEffectInterface;
 import it.polimi.ingsw.model.player.FamilyMember;
 import it.polimi.ingsw.utils.Debug;
 
@@ -8,9 +10,44 @@ import it.polimi.ingsw.utils.Debug;
  */
 public class TowerFloorAS extends AbstractActionSpace {
 
-    //TODO:
+    private int diceCost;
+
+    private ImmediateEffectInterface effect;
+
+    private AbstractCard card;
+
+    private FamilyMember familyMember;
+
+    public TowerFloorAS(int diceCost, ImmediateEffectInterface effect){
+        this.diceCost = diceCost;
+        this.effect = effect;
+    }
+
+    public void placeCard(AbstractCard card){
+        this.card = card;
+        clearFloor();
+    }
+
+    public AbstractCard getCard(){
+        return card;
+    }
+
     public void performAction(FamilyMember familyMember){
+
         this.doEffect();
+        this.familyMember = familyMember;
+    }
+
+    private void clearFloor(){
+        this.familyMember = null;
+    }
+
+    public int getDiceCost(){
+        return diceCost;
+    }
+
+    public FamilyMember getFamilyMember(){
+        return  familyMember;
     }
 
     private void doEffect(){
