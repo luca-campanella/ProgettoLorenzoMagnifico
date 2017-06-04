85165ea79c0857296e7b82169cae1f77c85d4492
Luca Campanella <luca.campanella@mail.polimi.it>
2017-06-04 17:04:19 +0200

    Modified CliOptionHandler

--- src/main/java/it/polimi/ingsw/client/CliOptionsHandler.java
+++ src/main/java/it/polimi/ingsw/client/CliOptionsHandler.java
@@ -1,37 +1,65 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.model.effects.immediateEffects.ImmediateEffectInterface;
-import it.polimi.ingsw.model.effects.immediateEffects.TakeOrPaySomethingEffect;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * this class handles all options 
+ * This class is used to ask the user a choice between multiple options, described in a String
  */
 
 public class CliOptionsHandler {
-    ArrayList<String> options = new ArrayList<String>();
-    Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<String> options;
+    private Scanner scanner = new Scanner(System.in);
 
     /**
-     * this is the constructor of the class. Since it's a view class, it only manages Strings
+     * this is the constructor of the class used to ask choices between effects.
      * @param effectOptions so when a list of effects comes, it translates that in strings
      */
-
-    public CliOptionsHandler(ArrayList<? extends ImmediateEffectInterface> effectOptions){
+    /*public CliOptionsHandler(ArrayList<ImmediateEffectInterface> effectOptions){
+        options = new ArrayList<String>(effectOptions.size());
         for(int i=0; i<effectOptions.size(); i++)
             this.addOption(effectOptions.get(i).descriptionShortOfEffect() , i);
+    }*/
+
+    /**
+     * Constructor with all the options already in place
+     * @param options
+     */
+    public CliOptionsHandler(ArrayList<String> options) {
+        this.options = options;
     }
 
     /**
-     * this method add options to the choice.
-     * @param option is the String list of options that someone has
-     * @param indexOfOption it's the index of the option
+     * this constructor should be called when we already know how many options we'll add later on
+     * @param numberOfOptions
      */
-    //todo: c'è corrispondenza tra questo indice e quello passato? --Arto
-    public void addOption(String option, int indexOfOption){
+    public CliOptionsHandler(int numberOfOptions) {
+        options = new ArrayList<>(numberOfOptions);
+    }
+
+    /**
+     * this constructor should be called only when no information on how many options we are going to add
+     */
+    public CliOptionsHandler() {
+        options = new ArrayList<>(2);
+
+    }
+
+    /**
+     * this method modifies an option.
+     * @param option is the String list of options that someone has
+     */
+    public void modifyOption(String option, int indexOfOption){
         options.set(indexOfOption, option);
+    }
+
+    /**
+     * Adds an option to the end of the list
+     * @param option description of the option
+     */
+    public void addOption(String option) {
+        options.add(option);
     }
 
     /**
@@ -39,12 +67,18 @@ public class CliOptionsHandler {
      * @return it returns the index of the option
      */
     public int askUserChoice(){
-        int numberPicked = options.size() + 2;
-        System.out.println("Select an option typing the right number");
+        int numberPicked;
+        System.out.println("Select an option typing the corresponding number");
         for(int i = 0; i< options.size(); i++)
-            System.out.println("Option (" + i + ")" + options.get(i));
-        while(numberPicked > options.size())
+            System.out.println(i + "--- " + options.get(i));
+
+        numberPicked = scanner.nextInt();
+
+        while(numberPicked > options.size()) {
+            System.out.println("Please insert a valid number, between 0 and " + options.size());
             numberPicked = scanner.nextInt();
+        }
+
         return numberPicked;
     }
 }
