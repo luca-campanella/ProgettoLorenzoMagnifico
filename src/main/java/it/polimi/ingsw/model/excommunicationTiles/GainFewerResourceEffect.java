135bb7644a79d995e4e1a3b708baf4316f7559f5
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2017-06-05 16:42:33 +0200

    implementing excommunication tiles

---
+++ src/main/java/it/polimi/ingsw/model/excommunicationTiles/GainFewerResourceEffect.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model.excommunicationTiles;
+
+import it.polimi.ingsw.model.resource.Resource;
+
+/**
+ * this class models the first period excommunications tile that gives you fewer resources each time you take some resources.
+ */
+public class GainFewerResourceEffect {
+    //type of resource effected by the excommunication and amount of excommunication
+    Resource resourceExcommunication;
+
+}
