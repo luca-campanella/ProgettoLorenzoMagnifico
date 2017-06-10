d1868485ab393b8926d15f22a7f43764252f6314
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2017-06-10 15:27:44 +0200

    removed modelControllerTest.

---
+++ src/test/java/it/polimi/ingsw/model/excommunicationTiles/BuildMalusEffectTest.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.model.excommunicationTiles;
+
+import it.polimi.ingsw.model.cards.Deck;
+import it.polimi.ingsw.server.JSONLoader;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testing buildMalusEffect
+ */
+public class BuildMalusEffectTest {
+    JSONLoader jsonLoader;
+    ArrayList<ExcommunicationTile> excommunicationTiles;
+
+    /**
+     * we load the excommunication tiles necessary for the test
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        jsonLoader = new JSONLoader();
+        excommunicationTiles = jsonLoader.loadExcommunicationTiles();
+
+    }
+    @Test
+    public void buildMalusEffect() throws Exception {
+        int valueTest;
+        valueTest = excommunicationTiles.get(4).effect.buildMalusEffect();
+        assertEquals(3, valueTest);
+
+    }
+
+}
