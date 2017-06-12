70816d409e80d6f992dc674a769f77401e5177d4
Luca Campanella <luca.campanella@mail.polimi.it>
2017-06-12 22:10:34 +0200

    debugging dicespacket

--- src/main/java/it/polimi/ingsw/client/network/socket/packet/DicesPacket.java
+++ src/main/java/it/polimi/ingsw/client/network/socket/packet/DicesPacket.java
@@ -2,19 +2,19 @@ package it.polimi.ingsw.client.network.socket.packet;
 
 import it.polimi.ingsw.model.board.Dice;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * the packet to deliver the dices to the player
  */
-public class DicesPacket {
+public class DicesPacket implements Serializable {
 
     private ArrayList<Dice> dices;
 
     public DicesPacket(ArrayList<Dice> dices){
-
-        this.dices.addAll(dices);
-
+        dices = new ArrayList<>(dices);
+        dices.forEach(dice -> System.out.println("Dice color " + dice.getColor() + "value: " + dice.getValue()));
     }
 
     public ArrayList<Dice> getDices(){
