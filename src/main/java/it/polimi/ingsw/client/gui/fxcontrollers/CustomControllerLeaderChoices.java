98b94d9bbcf89403581e304609ac5c310ad1894e
Luca Campanella <luca.campanella@mail.polimi.it>
2017-06-23 16:27:08 +0200

    worked on leader selector

--- src/main/java/it/polimi/ingsw/client/gui/fxcontrollers/CustomControllerLeaderChoices.java
+++ src/main/java/it/polimi/ingsw/client/gui/fxcontrollers/CustomControllerLeaderChoices.java
@@ -3,11 +3,17 @@ package it.polimi.ingsw.client.gui.fxcontrollers;
 import it.polimi.ingsw.model.leaders.LeaderCard;
 import it.polimi.ingsw.utils.Debug;
 import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+
+import java.util.HashMap;
 
 /**
  * Created by campus on 23/06/2017.
@@ -17,9 +23,17 @@ public class CustomControllerLeaderChoices extends CustomFxController {
     @FXML
     HBox leaderContainer;
 
+    @FXML
+    Button chooseButton;
+
+    HashMap<String, LeaderCard> buttonsLeadersMap = new HashMap<String, LeaderCard>(2);
+    ToggleGroup toggleGroup = new ToggleGroup();
+    LeaderCard selectedLeader;
+
     public void addLeader(LeaderCard leader) {
 
         final ToggleButton toggle = new ToggleButton();
+        toggle.setToggleGroup(toggleGroup);
        /* System.out.println(getClass());
         System.out.println(getClass().getResource("/LorenzoRitratto.jpg"));
         System.out.println(getClass().getResource("/Leaders/leaders_f_c_01"));*/
@@ -33,9 +47,23 @@ public class CustomControllerLeaderChoices extends CustomFxController {
         );
         toggleImage.setFitHeight(400);
         toggleImage.setPreserveRatio(true);
-        //toggle.setMaxHeight(100);
+
+        toggle.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                chooseButton.setDisable(false);
+                Object source = e.getSource();
+                if (source instanceof ToggleButton) { //should always be true
+                    ToggleButton clickedBtn = (ToggleButton) source; // that's the button that was clicked
+                    System.out.println(clickedBtn.getId()); // prints the id of the button
+                    selectedLeader = buttonsLeadersMap.get(clickedBtn.getId());
+                }
+            }
+        });
 
         leaderContainer.getChildren().add(toggle);
-        Debug.printVerbose("leader " + leader.getName() + "addedd scuccesfully to the window");
+        buttonsLeadersMap.put(toggle.getId(), leader);
+        Debug.printVerbose("leader " + leader.getName() + "added scuccesfully to the window");
     }
+
+
 }
