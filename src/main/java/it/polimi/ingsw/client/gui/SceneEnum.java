fe1d296e0910ad236f2b67c2b17300f271eb5cb8
Luca Campanella <luca.campanella@mail.polimi.it>
2017-06-28 09:00:03 +0200

    Modified thread waiting

---
+++ src/main/java/it/polimi/ingsw/client/gui/SceneEnum.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.client.gui;
+
+/**
+ * This enum helps {@link GraphicalUI} keep track in which window is in
+ * It contains the enumerations of all possible windows / scenes
+ */
+public enum SceneEnum {
+    CONNECTION_CHOICE,
+    LOGIN_REGISTER,
+    MAIN_BOARD
+}
